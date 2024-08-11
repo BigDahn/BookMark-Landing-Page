d22f40f21a00d5ad416ad09546157bcb27b56feb
BigDahn <harkinborholarholuwah@gmail.com>
2024-08-11 21:30:28 +0100

    Initail Commit

---
+++ src/Bookmark/data.js
@@ -0,0 +1,48 @@
+export const sublinks = [
+  {
+    id: 1,
+    image: '/Bookmark image/illustration-features-tab-1.svg',
+    title: 'Bookmark in one click',
+    info: 'Organize your bookmarks however you like. Our simple drag-and-drop interface gives you complete control over how you manage your favourite sites.',
+  },
+  {
+    id: 2,
+    image: '/Bookmark image/illustration-features-tab-2.svg',
+    title: 'Intelligent search',
+    info: 'Our powerful search feature will help you find saved sites in no time at all. No need to trawl through all of your bookmarks.',
+  },
+  {
+    id: 3,
+    image: '/Bookmark image/illustration-features-tab-3.svg',
+    title: 'Share your bookmarks',
+    info: 'Easily share your bookmarks and collections with others. Create a shareable link that you can send at the click of a button. ',
+  },
+]
+export const data = [
+  { id: 1, link: 'Simple Bookmarking' },
+  { id: 2, link: 'Speedy Searching' },
+  { id: 3, link: 'Easy Sharing' },
+]
+
+export const questions = [
+  {
+    id: 1,
+    title: 'What is Bookmark?',
+    info: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce tincidunt justo eget ultricies fringilla. Phasellus blandit ipsum quis quam ornare mattis.',
+  },
+  {
+    id: 2,
+    title: 'How can I request a new browser?',
+    info: 'Vivamus luctus eros aliquet convallis ultricies. Mauris augue massa, ultricies non ligula. Suspendisse imperdiet. Vivamus luctus eros aliquet convallis ultricies. Mauris augue massa, ultricies non ligula. Suspendisse imperdie tVivamus luctus eros aliquet convallis ultricies. Mauris augue massa, ultricies non ligula. Suspendisse imperdiet.',
+  },
+  {
+    id: 3,
+    title: 'Is there a mobile app?',
+    info: 'Sed consectetur quam id neque fermentum accumsan. Praesent luctus vestibulum dolor, ut condimentum urna vulputate eget. Cras in ligula quis est pharetra mattis sit amet pharetra purus. Sed sollicitudin ex et ultricies bibendum. ',
+  },
+  {
+    id: 4,
+    title: 'What about other Chromium browsers?',
+    info: 'Integer condimentum ipsum id imperdiet finibus. Vivamus in placerat mi, at euismod dui. Aliquam vitae neque eget nisl gravida pellentesque non ut velit.',
+  },
+]
